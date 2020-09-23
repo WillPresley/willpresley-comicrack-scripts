26e3f6c63f776d3b5a5dbf1803b9f08b79347950
Will Presley (sonata) <william.presley.jr+git@gmail.com>
2020-09-23 12:36:11 -0400

    Working script for ComicRack + Challenger Reader

--- Save-Cover-as-Image/WP-Save-Cover-as-Image_comicrack.py
+++ Save-Cover-as-Image/WP-Save-Cover-as-Image_comicrack.py
@@ -1,69 +1,22 @@
-#@Name  Save Cover as Image...
+#@Name  WP - Save Cover as Image
 #@Key   SaveCoverAsImage             [Defaults to function name]
 #@Hook  Books
 import clr
+
 from System import *
-from System.IO import *
+from System.IO import FileInfo, Path
+
 clr.AddReference("System.Drawing")
 from System.Drawing import *
 from System.Drawing.Imaging import *
-clr.AddReferenceByPartialName("System.Windows.Forms")
-from System.Windows.Forms import Clipboard, FolderBrowserDialog, DialogResult, MessageBox
 
 def SaveCoverAsImage(books):
-	folderDialog = FolderBrowserDialog()
-	folderDialog.Description = 'Select the destination to which selected eComics will be moved. \nExisting files in the destination folder will NOT be overwritten.'
-	folderDialog.ShowNewFolderButton = True
-	if folderDialog.ShowDialog(ComicRack.MainWindow) == DialogResult.OK:
-		destinationFolder = folderDialog.SelectedPath + "\\"
+	for book in books:
+		bookFileInfo = FileInfo(book.FilePath)
+		bookNameWithoutExt = Path.GetFileNameWithoutExtension(book.FilePath);
+		dirName = bookFileInfo.DirectoryName
 		
-		if Directory.Exists(destinationFolder):
-			for book in books:
-				pageNameString = "Cover"
-				destinationFilePath = destinationFolder + CreateComicName(book) + ' Page' + pageNameString + '.jpg'
-				
-				pageImage = ComicRack.App.GetComicPage(book, book.FrontCoverPageIndex)
-				pageImage.Save(destinationFilePath, ImageFormat.Jpeg)
-			
-def CreateComicName(book):
-
-	series = book.ShadowSeries
-	series = series.replace('?', '')
-	series = series.replace('/', '')
-	series = series.replace('\\', '')
-	series = series.replace('*', '')
-	series = series.replace(':', ' -')
-	series = series.replace('<', '[')
-	series = series.replace('>', ']')
-	series = series.replace('|', '!')
-	volume = ""
-	numeral = ""
-	count = ""
-	year = ""
-	if series != "":
-		if book.ShadowVolume != -1:
-			volume = " V" + str(book.ShadowVolume)
-			
-		if book.ShadowNumber != "":
-			try:
-				numeral = "%.3d" % int(book.ShadowNumber)
-				if (book.ShadowCount > 0) and (book.ShadowCount >= int(numeral)):
-					count = " (of " + "%.3d" % book.ShadowCount + ")"
-			except ValueError:
-				numeral = book.ShadowNumber
-				numeral = numeral.replace('?', '')
-				numeral = numeral.replace('/', ' ')
-				numeral = numeral.replace('\\', ' ')
-				numeral = numeral.replace('*', ' ')
-				numeral = numeral.replace(':', '-')
-				numeral = numeral.replace('<', '[')
-				numeral = numeral.replace('>', ']')
-				numeral = numeral.replace('|', '!')
-				numeral = numeral.replace('"', '\'').strip()
-			if len(numeral) > 0:
-				numeral = " " + numeral
-				
-		if book.ShadowYear > 1900:
-			year = " (" + str(book.ShadowYear) + ")"
-			
-		return series + volume + numeral + count + year
+		destinationFilePath = dirName + "\\" + bookNameWithoutExt + '.jpg'
+		
+		pageImage = ComicRack.App.GetComicPage(book, book.FrontCoverPageIndex)
+		pageImage.Save(destinationFilePath, ImageFormat.Jpeg)
